7edc7a7b9bfe508c53d356e351fa92914332a6ba
rahullahoria <rahul_lahoria@yahoo.com>
2016-08-10 20:10:32 +0530

    usage bug 1

--- web-site/index.php
+++ web-site/index.php
@@ -6,30 +6,25 @@
  * Time: 3:02 PM
  */
 
+//SELECT `user_id`,`date`,sum(time)/60/60 FROM `program_usage` WHERE 1 group by `user_id`,`date
+
+$db_handle = mysqli_connect("localhost","root","redhat@11111p","bulldog");
+$result = mysqli_query($db_handle,"SELECT `user_id`,`date`,sum(time)/60/60 as time FROM `program_usage` WHERE user_id=5 group by `user_id`,`date`;");
+$strData = "";
+while($row = mysqli_fetch_assoc($result)){
+
+    $strData .= "date: " .$row['date'] . " user_id: " . $row['user_id'] . " time in hrs: " . $row['time']  ;
+
+
+}
+
+mysqli_close($db_handle);
 ?>
-<h1>File-Dog <i>powered By Shatkon Labs</i></h1>
-<img src="logo.jpg" />
-
-<br/>
-
-<h2>Test it now</h2>
-<br/>
-1. Upload a file
-<br/>
-<form action="http://api.file-dog.shatkonlabs.com/files/rahul" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
-<br/>
-2. You will get ID put it with "http://api.file-dog.shatkonlabs.com/files/rahul/YOUR-FILE-ID"
-<br/>
-you will get the file...........
-<br/>
-have fun
-
-
-
-<br/>
-
-for any issue contact "anil@shatkonlabs.com"
+<div style="text-align: center;">
+    <img class="raleway-logo" src="http://shatkonlabs.com/images/logo.png">
+    <h1>Shatkon Labs Pvt. Ltd.</h1>
+    <img class="raleway-logo" src="https://upload.wikimedia.org/wikipedia/commons/1/13/Clyde_The_Bulldog.jpg">
+    <h2>Bulldog</h2>
+    <?= $strData ?>
+    <h4><i> if you have any problem contact at rahul@shatkonlabs.com or Call at 9599075955 </i></h4>
+</div>
