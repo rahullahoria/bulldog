75f12a3ec7d34aaccfbf5a74e449199ca314517a
rahullahoria <rahul_lahoria@yahoo.com>
2016-08-19 20:55:01 +0530

    making it by angular

--- api/resources/instance/getInstance.php
+++ api/resources/instance/getInstance.php
@@ -11,19 +11,14 @@ function getInstance($profession,$type){
     if ($type == "manager") $type = "white";
     if ($type == "employee") $type = "black";
 
-    $sql = "SELECT p.`pro_inst_id`, CONVERT(pi.name USING utf8)
-              FROM p_i as pi inner join `p_i_maps`as p
+    $sql = "SELECT p.`pro_inst_id`,pi.name
+              FROM `p_i_maps`as p inner join p_i as pi
                 WHERE p.`pro_inst_id` = pi.id and p.type='".$type."' and p.profession_id='".$profession."';";
 
-   /* $sql = "SELECT p.`pro_inst_id`, base64_encode(pix.`name`)
-FROM `p_i_maps` as p JOIN p_i as pix
-  WHERE p.`pro_inst_id` = pix.id and p.type='".$type."' and p.profession_id='".$profession."' ";*/
-
-
-    //die($sql);
+    //die($profession. " " . $type);
 
     try {
-        /*$db = getDB();
+        $db = getDB();
         $stmt = $db->prepare($sql);
 
         //$stmt->bindParam("type1", $type);
@@ -32,12 +27,7 @@ FROM `p_i_maps` as p JOIN p_i as pix
         //die(var_dump($stmt));
 
         $stmt->execute();
-        $instances = $stmt->fetchAll(PDO::FETCH_OBJ);*/
-
-        $db_handle = mysqli_connect("localhost", "root", "redhat@11111p", "bulldog");
-        $result = mysqli_query($db_handle, $sql);
-
-        for($instances = array(); $cost = mysqli_fetch_assoc($result); $instances[] = $cost);
+        $instances = $stmt->fetchAll(PDO::FETCH_OBJ);
 
         echo '{"instances": ' . json_encode($instances) . '}';
 
@@ -48,9 +38,6 @@ FROM `p_i_maps` as p JOIN p_i as pix
 
     } catch (PDOException $e) {
         //error_log($e->getMessage(), 3, '/var/tmp/php.log');
-        echo '{"error":{"text":' . $e->getMessage() . '}}';
-    }
-    catch (Exception $e){
         echo '{"error":{"text":' . $e->getMessage() . '}}';
     }
 }
