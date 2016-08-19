31ac34ab462ca1fbb9683a422aa5e4a15a5860f8
rahullahoria <rahul_lahoria@yahoo.com>
2016-08-19 15:58:46 +0530

    making it by angular

--- api/resources/instance/getInstance.php
+++ api/resources/instance/getInstance.php
@@ -15,7 +15,7 @@ function getInstance($profession,$type){
               FROM p_i as pi inner join `p_i_maps`as p
                 WHERE p.`pro_inst_id` = pi.id and p.type='".$type."' and p.profession_id='".$profession."';";
 
-    $sql = "SELECT p.`pro_inst_id`, pix.name
+    $sql = "SELECT p.`pro_inst_id`, pix.`name`
 FROM `p_i_maps` as p INNER JOIN p_i as pix
   WHERE p.`pro_inst_id` = pix.id and p.type='".$type."' and p.profession_id='".$profession."' ";
 
