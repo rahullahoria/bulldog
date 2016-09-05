2961589729f69f20be263727c9dc159709dd702b
rahullahoria <rahul_lahoria@yahoo.com>
2016-09-05 13:30:10 +0530

    making it by angular

--- api/resources/companies/managers/getManagerEmployees.php
+++ api/resources/companies/managers/getManagerEmployees.php
@@ -13,10 +13,14 @@ function getManagerEmployees($companyId, $managerId){
 
     $month = $app->request()->get('month');
 
-    $sql = "SELECT `user_id` ,u.name, u.md5_id, sum( time ) AS time
+    $sql = "SELECT p.`user_id` ,u.name, u.md5_id, sum( time ) AS time
             FROM `usages` as p inner join users as u
-            WHERE p.`user_id` = u.id and MONTH(p.creation) = :month
-            GROUP BY `user_id`; ";
+            WHERE
+              p.`user_id` = u.id
+              and MONTH(p.creation) = :month
+              and u.company_id = (select id from companies where name = :companyId)
+              and p.`user_id` in (select `user_employee_id` from manager_employee_mappings where user_manager_id = (select id from users where md5_id = :managerId))
+            GROUP BY p.`user_id`";
 
     try {
         $db = getDB();
@@ -25,6 +29,8 @@ function getManagerEmployees($companyId, $managerId){
         /*$stmt->bindParam("id", $fileId);
         $stmt->bindParam("username", $username)*/;
         $stmt->bindParam("month", $month);
+        $stmt->bindParam("managerId", $managerId);
+        $stmt->bindParam("companyId", $companyId);
 
         $stmt->execute();
         $employees = $stmt->fetchAll(PDO::FETCH_OBJ);
